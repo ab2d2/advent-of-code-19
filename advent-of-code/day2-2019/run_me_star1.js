0cba634dc1fdf1b1caa13cff017f7e96eb276b67
ab2d2 <abdul.rahimi_defra@outlook.com>
2020-01-28 13:18:04 +0000

    added a fix to config .env

--- advent-of-code/day2-2019/run_me_star1.js
+++ advent-of-code/day2-2019/run_me_star1.js
@@ -1,11 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const { processOpCodes } = require('./day2-star1')
 
 const { makeAPICall } = require("../../utils/index");
-
 makeAPICall(process.env.SESSION_KEY, handleDataFromAPI)
 
 function handleDataFromAPI(data) {
-	let inputFromAdventOfCode = data.split(',')
-	console.log("recieved ", inputFromAdventOfCode.length, " items")
+	const inputFromAdventOfCode = data.split(',')
+	// console.log("recieved ", inputFromAdventOfCode.length, " items")
+	const answer = processOpCodes(inputFromAdventOfCode)
+	console.log("answer for day2 star 1 is: ", answer)
 }
